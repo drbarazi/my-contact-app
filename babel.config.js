2b8ab853375196568437918a526495a7136c768c
drbarazi <muhammad.agessy.barazi@gmail.com>
2024-04-09 01:29:13 +0700

    first launch 🚀🚀🚀

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		'react-native-reanimated/plugin',
+		[
+			'module-resolver',
+			{
+				root: ['./src'],
+				extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+				alias: {
+					tests: ['./tests/'],
+					"@navigation": "./src/navigation",
+					"@screens": "./src/screens",
+					"@components": "./src/components",
+					"@theme": "./src/theme/index",
+					'@api': './src/api',
+					'@assets': './src/assets',
+				}
+			}
+		]
+	],
+};
